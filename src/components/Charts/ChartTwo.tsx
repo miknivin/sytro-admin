34dc0e140a61da01355adb2343f904aa7e76f6fc
miknivin <116654570+miknivin@users.noreply.github.com>
2025-02-24 13:08:19 +0530

    bug-resolved

--- src/components/Charts/ChartTwo.tsx
+++ src/components/Charts/ChartTwo.tsx
@@ -59,9 +59,9 @@ const options: ApexOptions = {
     fontWeight: 500,
     fontSize: "14px",
 
-    markers: {
-      radius: 99,
-    },
+    // markers: {
+    //   radius: 99,
+    // },
   },
   fill: {
     opacity: 1,
