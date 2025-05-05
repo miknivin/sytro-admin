d2821f34670e745956b771a6fbef950b626236a5
miknivin <116654570+miknivin@users.noreply.github.com>
2025-05-05 13:59:49 +0530

    insta message to db

--- src/app/api/orders/webhook/instagram/route.js
+++ src/app/api/orders/webhook/instagram/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
-import dbConnect from "@/lib/dbConnect"; // Adjust the path to your dbConnect file
+
 import Message from "../../../../../models/Message";
+import dbConnect from "@/lib/db/connection";
 
 export const dynamic = "force-dynamic";
 
